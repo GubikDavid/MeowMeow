ad6e91c9e032e36bef5fb1767ff3ea67a2f07d1f
PiletinaTime <davidgubik1@gmail.com>
2021-08-31 10:04:18 +0200

    kk

--- Assets/Scripts/FollowYarn.cs
+++ Assets/Scripts/FollowYarn.cs
@@ -4,14 +4,40 @@ public class FollowYarn : MonoBehaviour
 {
     [SerializeField]
     private GameObject target;
-    private float[] positions = { 1f, 1.1f, 1.184f, 1.152f, 1.264f };
-    private float[] rotations = { 25.686f, 21.189f, 13.327f, 3.07f, 0f, -0.229f, -3.855f, -9.909f, -13.367f };
+    private float speed = 12f;
+    private float[] positions = {1f, 1.1f, 1.184f, 1.152f, 1.264f};
+    private float[] rotations = {25.686f, 21.189f, 13.327f, 3.07f, 0f, -0.229f, -3.855f, -9.909f, -13.367f};
+    private void Start()
+    {
+        Physics.IgnoreCollision(transform.GetChild(1).GetComponent<Collider>(), target.GetComponent<Collider>());
+    }
     void Update()
     {
+        
+        //FollowX();
+        transform.position = new Vector3(target.transform.position.x, transform.position.y, transform.position.z);
         int num = (int)(Health.health / 20) - 1;
-        if (num <= 4) transform.position = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z - .858f);
-        else transform.position = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z - (num <= 9 ? positions[num - 5] : target.transform.localScale.x / 2 + .23f));
-        Vector3 eulers = transform.rotation.eulerAngles;
-        transform.rotation = Quaternion.Lerp(Quaternion.Euler(transform.rotation.eulerAngles), Quaternion.Euler(new Vector3(num < 8 ? rotations[num] : rotations[8], eulers.y, eulers.z)), Time.deltaTime / .8f);
+        float step = speed * Time.deltaTime;
+/*        if (num <= 4)
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z - .858f), 50f * Time.deltaTime);// transform.position = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z - .858f);
+        else transform.position = Vector3.MoveTowards(transform.position, new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z - (num <= 9 ? positions[num - 5] : target.transform.localScale.x / 2 + .23f)), 50f * Time.deltaTime);*/
+        //else transform.position = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z - (num <= 9 ? positions[num - 5] : target.transform.localScale.x / 2 + .23f));
+        if (num <= 4) transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y, target.transform.position.z - .858f), step);
+        else transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y, target.transform.position.z - (num <= 9 ? positions[num - 5] : 3.23f)), step);
+        transform.rotation = Quaternion.Lerp(Quaternion.Euler(transform.rotation.eulerAngles), Quaternion.Euler(new Vector3(num < 8 ? rotations[num] : rotations[8], transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z)), Time.deltaTime / .8f);
+    }
+    /*private void FollowX()
+    {
+        transform.position = new Vector3(target.transform.position.x, transform.position.y, transform.position.z);
+    }*/
+    public void Plus() 
+    {
+        if(speed < 11) speed++;
+        print(speed);
+    }
+    public void minus() 
+    {
+        speed--;
+        print(speed);
     }
 }
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public static float speed = 10;
-    private readonly float speedModifier = 45f;
+    //private readonly float speedModifier = 45f;
+    private readonly float speedModifier = 30000f;
     private Rigidbody rb;
     private Vector3 newPosition;
+    //private float move;
+    private Vector3 pastPosition;
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
@@ -18,7 +21,7 @@ public class Movement : MonoBehaviour
     {
         if (GameManager.startGame)
         {
-            if (Input.touchCount > 0)
+            /*if (Input.touchCount > 0)
             {
                 Touch touch = Input.GetTouch(0);
                 switch (touch.phase)
@@ -31,6 +34,41 @@ public class Movement : MonoBehaviour
                         newPosition.x = 0;
                         break;
                 }
+                rb.velocity = Vector3.ClampMagnitude(new Vector3(newPosition.x, rb.velocity.y, rb.velocity.z), 60f);
+            }*/
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+                switch (touch.phase)
+                {
+                    case TouchPhase.Moved:
+                        Vector2 dragDistance = new Vector2(touch.deltaPosition.x / Screen.width, touch.deltaPosition.y / Screen.height);
+                        newPosition = speedModifier * Time.deltaTime * dragDistance * Vector3.right;
+                        break;
+                    case TouchPhase.Stationary:
+                    case TouchPhase.Ended:
+                        newPosition.x = 0;
+                        break;
+                }
+
+                /*switch (touch.phase)
+                {
+                    Touch touch = Input.GetTouch(0);
+                    case TouchPhase.Began:
+                        pastPosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+                        break;
+                    case TouchPhase.Moved:
+                        newPosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+                        move = (newPosition.x - pastPosition.x) * speedModifier * Time.deltaTime;
+                        pastPosition = newPosition;
+                        break;
+                    case TouchPhase.Ended:
+                    case TouchPhase.Stationary:
+                    case TouchPhase.Canceled:
+                        move = 0;
+                        //rb.angularVelocity = Vector3.zero;
+                        break;
+                }*/
                 rb.velocity = Vector3.ClampMagnitude(new Vector3(newPosition.x, rb.velocity.y, rb.velocity.z), 60f);
             }
             MoveForward();
